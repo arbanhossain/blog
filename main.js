106303c655bfef6f602048945a0727523f67b5ea
arbanhossain <arbanhossain@gmail.com>
2020-04-28 14:37:00 +0600

    better routing and little custom styling

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
+const main_body = document.getElementById("main-body");
 const postListDiv = document.getElementById("postlist");
 const postDiv = document.getElementById("post");
 const converter = new showdown.Converter();
 
-document.body.onload = () => {
+let root = null;
+let useHash = true;
+let hash = '#';
+let router = new Navigo(root, useHash, hash);
+
+/*document.body.onload = () => {
     getPostList();
-}
+}*/
 
 const getPostList = () => {
     let text = ``;
     posts.forEach(item => {
-        text += `<a href="#" id="postlink" data-link="${item.link}" onclick="fetchPost(this)">${item.name}</a><br>`;
+        text += `<p class="postlink" data-link="${item.link}" onclick="router.navigate('/${item.link}')">• ${item.name}</p><br>`;
     })
-    postListDiv.innerHTML = text;
+    main_body.innerHTML = text;
 }
 
-const fetchPost = (e) => {
+const fetchPost = (link) => {
     //console.log(e);
-    let link = e.dataset.link;
-    console.log(link);
+    //let link = e.dataset.link;
+    //console.log(link);
     fetch(`./posts/${link}`, {mode:'no-cors'})
     .then(res => res.text())
     .then(data => {
-        console.log(data);
-        let html = converter.makeHtml(data);
-        console.log(html);
-        postDiv.innerHTML = html;
+        //console.log(data);
+        let html = `<p class='custom-link' onclick="router.navigate('/')">< go home</p>`
+        html += converter.makeHtml(data);
+        //console.log(html);
+        main_body.innerHTML = html;
     })
     .catch(err => console.error(err));
 }
+
+router.on(() => {
+    getPostList();
+})
+.resolve();
+
+router.on({
+    '/:id': (params) => {
+        fetchPost(params.id+'.md');
+        console.log('Loaded!');
+    }
+})
+.resolve();
+
+router.notFound(() => {
+    console.log('No such page! :o');
+})
